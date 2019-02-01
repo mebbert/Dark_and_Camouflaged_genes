294a47fc7e93d6feba6e93c1affc7c01ed0c2538
Tanner Jensen <tanner.jensen08@gmail.com>
2019-02-01 10:30:42 -0600

    Added GATK path as an entry point to step 07

--- scripts/07_RUN_ADSP/submit.sh
+++ scripts/07_RUN_ADSP/submit.sh
@@ -7,10 +7,13 @@
 REALIGN="../../results/illuminaRL100/b37/illuminaRL100.b37.camo.realign.sorted.bed" # realign camo bed created in step 5
 ALIGN_TO="../../results/illuminaRL100/b37/illuminaRL100.b37.camo.align_to.sorted.expanded_50.bed" #align_to camo bed created in step 5 and expanded by 50 in step 6
 
-#REF_PREFIX = "${MASK_RESULT_DIR}/${MASK_REF_PREFIX}" from Step 6
+GATK_JAR="GenomeAnalysisTK.jar" #path to GATK jar (version 3.8)
+
+# REF_PREFIX is equal to "${MASK_RESULT_DIR}/${MASK_REF_PREFIX}" from Step 6
 REF_PREFIX="../../results/b37_camo_mask/b37-camo_mask" # Path prefix to the camo-masked seperate ploidy references
 FILTERED_BAM_LIST="ADSP_filtered_bams.list" # List of paths to ADSP (or other dataset) bams that passed QC, one per line
 DATA_DEST_DIR="../../results/ADSP" #Where to store the intermediate gVCFs results 
+
 ##################################################################
 
 ##Run Filtered Samples
@@ -23,7 +26,8 @@ do
 		$i,$((i + 1)) \
 		$REALIGN \
 		$ALIGN_TO \
+		$GATK_JAR \
 		$REF_REFIX \
 		$FILTERED_BAM_LIST \
-		$DATA_DEST_DIR
+		$DATA_DEST_DIR 
 done
--- scripts/08_COMBINE_AND_GENOTYPE_ADSP/submit.sh
+++ scripts/08_COMBINE_AND_GENOTYPE_ADSP/submit.sh
@@ -8,7 +8,7 @@ GVCF_RESULT_DIR="../../results/ADSP/gvcfs" # Dir created in step 7 that contains
 ALIGN_TO_BED="../../results/illuminaRL100/b37/illuminaRL100.b37.camo.align_to.sorted.bed" # Camo align_to bed created in step 5
 RESULT_DIR="../../results/ADSP" # where to write the combined vcfs
 CAMO_MASK_REF_PREFIX="../../b37_camo_mask/b37-camo_mask" #prefix to all the masked genomes created in step 6
-GATK_JAR="GenomeAnalysisToolKit.3.8.1.jar" # Path to GATK jar
+GATK_JAR="GenomeAnalysisToolKit.3.8.1.jar" # Path to GATK jar (version 3.8)
 ##################################################################
 
 bash submit_combine_and_genotype.ogs \
